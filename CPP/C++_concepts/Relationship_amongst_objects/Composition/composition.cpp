7555be71b3ed378162e5e9087c80536117461ef5
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2026-07-23 21:36:34 +0530

    Update composition.cpp

--- CPP/C++_concepts/Relationship_amongst_objects/Composition/composition.cpp
+++ CPP/C++_concepts/Relationship_amongst_objects/Composition/composition.cpp
@@ -1,65 +1,97 @@
-/*
-Composition(composed inside other class):  Type of association. Dependent. 
-One object will carry another object as a value. One object destroy, 
-other will also destroy. Strong relationship.
-Example: Car object has Engine object. If Car object is destroyed, 
-Engine object will be destroyed.
-*/
-
 #include <iostream>
 #include <string>
+
 using namespace std;
+
 class Engine
 {
-	int power;
-	public:
-	Engine(){}
-	Engine(int power):power(power)
-	{
-		cout<<"Engine object is created"<<endl;
-	}
-	Engine(const Engine& e)
-	{
-		cout<<"Engine::Copy ctor is called"<<endl;
-	}
-	Engine& operator=(const Engine& e)
-	{
-		cout<<"Engine::assignment operator is called"<<endl;
-	}
-	~Engine()
-	{
-		cout<<"Engine object is destroyed"<<endl;
-	}
+    int power;
+
+public:
+    Engine()
+    {
+        cout << "Engine Default Constructor\n";
+    }
+
+    Engine(int power) : power(power)
+    {
+        cout << "Engine Parameterized Constructor\n";
+    }
+
+    Engine(const Engine& e) : power(e.power)
+    {
+        cout << "Engine Copy Constructor\n";
+    }
+
+    Engine& operator=(const Engine& e)
+    {
+        cout << "Engine Assignment Operator\n";
+
+        if (this != &e)
+            power = e.power;
+
+        return *this;
+    }
+
+    ~Engine()
+    {
+        cout << "Engine Destructor\n";
+    }
 };
+
 class Car
 {
-	int model;
-	string name;
-	Engine eng;
-	public:
-	//If we don't call Engine::Engine(const Engine&), then Engine::Engine() will be called
-	//to initialize Engine object of class Car.
-	Car(string name, int model, Engine eng1): name(name), model(model),eng(eng1) //will call copy ctor of Engine
-	{
-		//eng = eng1; //will call assignment operator
-		cout<<"Car object is created"<<endl;
-	}
-	Car(const Car& c):eng(c.eng)///will call copy ctor of Engine
-	{
-		//eng = c.eng; //will call assignment operator
-		cout<<"Car object is created"<<endl;
-	}
-	~Car()
-	{
-		cout<<"Car object is destroyed"<<endl;
-	}
+    int model;
+    string name;
+    Engine eng;
+
+public:
+
+    Car(string name, int model, const Engine& eng1)
+
+        // ==============================
+        // Option 1 (Recommended)
+        // Calls Engine Copy Constructor
+        // ==============================
+        : model(model), name(name), eng(eng1)
+
+        // ==============================
+        // Option 2
+        // Uncomment this initializer and
+        // comment the above initializer.
+        //
+        // Engine Default Constructor
+        // will be called first because
+        // eng isn't initialized.
+        //
+        // Then assignment operator will
+        // be called inside constructor.
+        // ==============================
+        // : model(model), name(name)
+    {
+        // Uncomment this only when using
+        // Option 2 above.
+
+        // eng = eng1;      // Engine Assignment Operator
+
+        cout << "Car Constructor\n";
+    }
+
+    ~Car()
+    {
+        cout << "Car Destructor\n";
+    }
 };
+
 int main()
 {
-	Engine e(10);
-	/*Car *c = new Car ("BMW", 134, e);
-	delete c;*/
-	Car c("BMW", 134, e);
-	Car c1 = c;
-	return 0;
+    Engine e(100);
+
+    cout << "-------------------------\n";
+
+    Car c("BMW",101,e);
+
+    cout << "-------------------------\n";
+
+    return 0;
 }
