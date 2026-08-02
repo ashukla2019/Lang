694150c037df4b113d797574a9c8bf79c1b42fe5
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2026-08-02 22:33:58 +0530

    Update singleton.cpp

--- CPP/Design_pattern/Creational_design_pattern/singleton.cpp
+++ CPP/Design_pattern/Creational_design_pattern/singleton.cpp
@@ -1,89 +1,38 @@
-/*
-Creational Design Patterns: Creational patterns provide various object creation mechanisms, 
-which increase flexibility and reuse of existing code.
-Singleton: Define class that has only one instance and provides global point of access.
-Requirement: one instance, global access, No ownership
-Advantages: Save memory, Single access point(logger(error logging), database connection(single connection point to avoid corrupting data base), flexibility(change anytine)
-Usage: Database application, Config settings(game settings, Application settings etc.)
-*/
-
-/*
-Difference b/w static class and singleton:
-Main differences are:
-Singleton has an instance/object while static class is a bunch of static methods.
-Singleton can be inherited which supports open/close principles in SOLID principles on the other hand static class can't be inherited and we need to make changes in itself.
-Singleton object can be passed to methods while static class as it does not have instance can't be passed as parameters
-*/
-
 #include <iostream>
-#include <string>
-
 using namespace std;
 
-class GameSetting
-{
-	static GameSetting* _instance;
-	int _brightness;
-	int _width;
-	int _height;
-	GameSetting() : _width(786), _height(1300), _brightness(75){}
-	// all constructors should be private or public(iff you want to allow inheritance)
+class Singleton {
+private:
+    // Private constructor
+    Singleton() {
+        cout << "Singleton object created\n";
+    }
+
+    // Prevent copying
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
 
 public:
-	static GameSetting* getInstace() 
-	{
-		if(_instance == NULL) 
-			_instance = new GameSetting();
-		return _instance;
-	}
-	void setWidth(int width) 
-	{
-		_width = width;
-	}
-	void setHeight(int height) 
-	{
-		_height = height;
-	}
-	void setBrighness(int brightness) 
-	{
-		_brightness = brightness;
-	}
+    // Global access point
+    static Singleton& getInstance() {
+        static Singleton instance;  // Created only once
+        return instance;
+    }
 
-	int getWidth() 
-	{
-		return _width;
-	}
-	int getHeight() 
-	{
-		return _height;
-	}
-	int getBrightness() 
-	{
-		return _brightness;
-	}
-	void displaySetting() 
-	{
-		cout << "brightness: " << _brightness << endl;
-		cout << "height: " << _height << endl;
-		cout << "width: " << _width << endl << endl;
-	}
+    void display() {
+        cout << "Hello from Singleton\n";
+    }
 };
 
-GameSetting * GameSetting::_instance = NULL;
+int main() {
+    Singleton& obj1 = Singleton::getInstance();
+    Singleton& obj2 = Singleton::getInstance();
 
-void someFunction () 
-{
-	GameSetting *setting = GameSetting::getInstace();
-	setting->displaySetting();
-}
+    obj1.display();
+    obj2.display();
 
-int main() 
-{
+    if (&obj1 == &obj2)
+        cout << "Both objects are the same instance.\n";
 
-	GameSetting *setting = GameSetting::getInstace();
-	setting->displaySetting();
-	setting->setBrighness(100);
-	
-	someFunction();
-	return 0;
+    return 0;
 }
